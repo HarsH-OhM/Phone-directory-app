01432665173a006762d47ffc939d5918fb8c5f9e
Harsh <gurjarhariom3114@gmil.com>
2020-02-08 01:21:08 +0530

    Subscriber contact page

---
+++ src/ShowSubscribers.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react';
+import Header from './Header';
+import './ShowSubscribers.css';
+import {Link} from 'react-router-dom';
+
+class ShowSubscribers extends Component {
+
+
+   onDeletedClick = (subscriberId) => {
+      this.props.deleteSubscriberHandler(subscriberId);
+   }
+
+//    constructor(){
+      
+//    super();
+//    this.state ={
+//       subscribersListToShow:[]
+//    }
+// }
+
+//    clickHandler(message){
+//       alert("delete  clicked");
+//    }
+
+
+  
+   render(){
+     
+
+   //  let subscribers = [
+   //    {
+   //       id:1, // unique
+   //       name: "Kajol",
+   //       phone: '9876543217'
+   //    },
+   //    {
+   //       id:2, // unique
+   //       name: "arav",
+   //       phone: '1234567899'
+   //    }
+   // ];
+   return (
+      
+    <div>
+     <Header heading="MY-PHONE DIRECTORY"/>
+        <div className="component-body-container">
+          <Link to="/add"><button className="custom-btn add-btn">Add</button></Link>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          
+          {
+       this.props.subscribersList.map(sub => {
+          return <div key= {sub.id} className="grid-container">
+             <span className="grid-item">{sub.name}</span>
+            <span className="grid-item">{sub.phone}</span>
+            <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn"  onClick={this.onDeletedClick.bind(this, sub.id)}>Delete</button>
+                </span>
+      	
+      </div>
+   })
+}
+          
+          </div>
+    </div>
+  );
+   }
+
+}
+
+export default ShowSubscribers;
